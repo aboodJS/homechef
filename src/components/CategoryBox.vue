e2acd10afecec79e13315fc7f66cd822fa81ad7d
ajs <140010746+aboodJS@users.noreply.github.com>
2026-03-10 17:42:54 +0300

    added a simple sidebar, get the categories list from the API

---
+++ src/components/CategoryBox.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+const props = defineProps(["title", "image", "id"])
+
+</script>
+
+<template>
+  <li  :id="props.id" class="flex w-56  items-center border border-[#e6e7eb] rounded-lg cursor-pointer"><img class="w-24 relative z-0" :src="props.image" alt=""><p>{{ props.title }}</p></li>
+</template>
---
+++ src/pages/Home.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import CategoryBox from '@/components/CategoryBox.vue';
+import { ref } from 'vue';
+import { onMounted } from 'vue';
+
+const categories = ref("")
+
+
+onMounted(async () => {
+  await fetch("https://www.themealdb.com/api/json/v1/1/categories.php").then(d => d.json()).then((final) => categories.value = final.categories)
+
+})
+
+</script>
+
+<template>
+<ul class="text-[#dee0e4] grid gap-y-6 content-center">
+  <h1 class="text-2xl font-bold font-[Playfair-Display]">Categories</h1>
+  <CategoryBox v-for="categorey in categories" :title="categorey.strCategory" :image="categorey.strCategoryThumb" :key="categorey.idCategory"></CategoryBox>
+</ul>
+</template>
