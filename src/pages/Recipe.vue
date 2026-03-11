e5fe257ed8abff60a7ce8493b751d2e6da07abf7
ajs <140010746+aboodJS@users.noreply.github.com>
2026-03-11 19:54:38 +0300

    added more information to the details page

--- src/pages/Recipe.vue
+++ src/pages/Recipe.vue
@@ -5,25 +5,47 @@ import { useRoute } from 'vue-router';
 const route = useRoute()
 
 const details: Ref = ref([])
+const ingrediants: Ref = ref([])
+
+
 
 
 onMounted(async () => {
   await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${route.params.id}`).then(d => d.json()).then(final => {
     details.value = final.meals[0]
   }).catch(err => console.log(err))
+
+  for (const key in details.value) {
+    if (key.match("strIngredient")  != null) {
+      if (details.value[key] !== "") {
+        ingrediants.value.push(details.value[key])
+      }
+
+    }
+
+  }
 })
 
 </script>
 
 <template>
-  <section class="text-white">
-    <h1 >{{ details.strMeal }}</h1>
-    <p>Origin: {{ details.strArea }}</p>
+  <section class="text-white grid gap-6">
+    <div>
+      <h1 class="font-bold text-2xl">{{ details.strMeal }}</h1>
+      <p class="font-bold">Origin: {{ details.strArea }}</p>
+
+    </div>
     <div>
       <p>Instructions</p>
       <p>{{ details.strInstructions }}</p>
 
     </div>
+       <div class="gap-3 grid">
+         <h1 class="font-bold text-xl">ingrediants</h1>
+               <ul class="list-disc">
+                  <li :key="i"  v-for="ing,i in ingrediants">- {{ ing }}</li>
+               </ul>
+       </div>
 
   </section>
 </template>
