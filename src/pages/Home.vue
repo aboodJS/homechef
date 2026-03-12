7d90854a5433c96619dc54f5b8baf60cef188e86
ajs <140010746+aboodJS@users.noreply.github.com>
2026-03-12 15:07:10 +0300

    made the recipe page look better,made the home page look better

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -24,17 +24,17 @@ onMounted(async () => {
 </script>
 
 <template>
-  <section class="flex">
+  <section class="grid grid-rows-3 grid-cols-5">
   <div class="grid gap-y-4">
   <h1 class="text-[#e6e6e9] text-2xl font-bold font-[Playfair-Display]">Categories</h1>
   <ul class="text-[#dee0e4] grid gap-y-3 content-center top-0 w-fit">
   <CategoryBox @click="() => {
     currentCategorey = categorey.strCategory
     getMeals(categorey.strCategory)
-  }" v-for="categorey in categories.slice(0, 6)" :title="categorey.strCategory" :image="categorey.strCategoryThumb" :key="categorey.idCategory"></CategoryBox>
+  }" v-for="categorey in categories.slice(0, 8)" :title="categorey.strCategory" :image="categorey.strCategoryThumb" :key="categorey.idCategory"></CategoryBox>
   </ul>
   </div>
-  <div class="grid gap-4 grid-rows-2 grid-cols-3">
+  <div class="grid grid-rows-2 gap-3 grid-cols-4 row-span-1 col-span-4 self-center">
     <RecipeBox v-for="meal in results.splice(0,6)" :name="meal.strMeal" :image="meal.strMealThumb" :meal-id="meal.idMeal"></RecipeBox>
   </div>
   </section>
--- src/pages/Recipe.vue
+++ src/pages/Recipe.vue
@@ -29,23 +29,24 @@ onMounted(async () => {
 </script>
 
 <template>
-  <section class="text-white grid gap-6">
-    <div>
+  <section class="text-white grid justify-center gap-6">
+    <div class=" w-[90vw]  text-center grid gap-5">
       <h1 class="font-bold text-2xl">{{ details.strMeal }}</h1>
       <p class="font-bold">Origin: {{ details.strArea }}</p>
-
+      <img class="w-64 rounded-lg object-cover justify-self-center" :src="details.strMealThumb" alt="">
     </div>
-    <div>
-      <p>Instructions</p>
+       <div class="gap-3 w-[90vw]   text-center grid">
+         <h1 class="font-bold text-xl">ingrediants</h1>
+               <ul>
+                  <li :key="i"  v-for="ing,i in ingrediants">{{ ing }}</li>
+               </ul>
+       </div>
+    <div class="text-center border w-[90vw] p-3 border-gray-100 rounded-lg bg-[#394050b1] ">
+      <p class="font-bold text-2xl">Instructions</p>
       <p>{{ details.strInstructions }}</p>
 
     </div>
-       <div class="gap-3 grid">
-         <h1 class="font-bold text-xl">ingrediants</h1>
-               <ul class="list-disc">
-                  <li :key="i"  v-for="ing,i in ingrediants">- {{ ing }}</li>
-               </ul>
-       </div>
+
 
   </section>
 </template>
